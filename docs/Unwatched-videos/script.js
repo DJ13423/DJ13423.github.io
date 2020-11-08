b2a8f6a4491b33a7f0b2ba0c5c3592acb86731ff
DJ13423 <DJ13423@users.noreply.github.com>
2020-11-07 22:30:54 -0800

    update

--- docs/Unwatched-videos/script.js
+++ docs/Unwatched-videos/script.js
@@ -7,7 +7,7 @@ function loadNow(opacity) {
         loader.style.opacity = opacity;
         window.setTimeout(function() {
             loadNow(opacity - 0.01);
-        }, 90000);
+        }, 10000);
     }
 }
 
