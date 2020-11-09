08a22751f0d2010ff7b07b825f4bf98aef1b81ab
DJ13423 <DJ13423@users.noreply.github.com>
2020-11-08 19:47:44 -0800

    update

--- docs/index-script.js
+++ docs/index-script.js
@@ -7,7 +7,7 @@ function loadNow(opacity) {
         loader.style.opacity = opacity;
         window.setTimeout(function() {
             loadNow(opacity - 10);
-        }, 100000);
+        }, 500);
     }
 }
 
